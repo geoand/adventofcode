84f8107f5eaab1f58c586463e349c20dd69ae4a4
George Andrianakis <geoand@gmail.com>
2016-01-22 23:12:03 +0200

    Implemented part two of problem 14

--- src/main/groovy/geoand/adventofcode/problems/Problem14.groovy
+++ src/main/groovy/geoand/adventofcode/problems/Problem14.groovy
@@ -11,10 +11,24 @@ final int RUNNING_TIME = 2503
 
 final List<String> lines = InputProviderFactory.inputProvider().getLines(14)
 
-println lines
-        .collect {Reindeer.fromInput(it)}
-        .max {it.distanceCovered(RUNNING_TIME)}
-        .distanceCovered(RUNNING_TIME)
+final List<Reindeer> reindeerList = lines .collect { Reindeer.fromInput(it) }
+println reindeerList.collect {it.distanceCovered(RUNNING_TIME)}.max()
+
+
+
+final List<Integer> initialScores = (1..reindeerList.size()).collect { 0 }
+println (((1..RUNNING_TIME).inject(initialScores) { List<Integer> runningScores, int time ->
+    final distanceListForTime = reindeerList.collect{reindeer ->
+        reindeer.distanceCovered(time)
+    }
+
+    final maxScoreOfRound = distanceListForTime.max()
+    final List<Integer> roundScores = distanceListForTime.collect {it ==  maxScoreOfRound ? 1 : 0}
+
+    [runningScores, roundScores].transpose().collect{it[0] + it[1]}
+}).max())
+
+
 
 
 
