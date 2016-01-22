6e47c01fa121dbd4230df1b950e8af1105e8c0d5
George Andrianakis <geoand@gmail.com>
2016-01-22 20:15:43 +0200

    Implemented part one of problem 14

--- src/main/groovy/geoand/adventofcode/problems/Problem14.groovy
+++ src/main/groovy/geoand/adventofcode/problems/Problem14.groovy
@@ -2,19 +2,19 @@ package geoand.adventofcode.problems
 
 import geoand.adventofcode.support.input.InputProviderFactory
 import geoand.adventofcode.support.model.reindeer.Reindeer
-import groovy.transform.PackageScope
-
-import java.util.regex.Matcher
 
 /**
  * Created by gandrianakis on 22/1/2016.
  */
 
+final int RUNNING_TIME = 2503
+
 final List<String> lines = InputProviderFactory.inputProvider().getLines(14)
 
-println lines.collect {Reindeer.fromInput(it)}.max {Reindeer a, Reindeer b -> a.distanceCovered(2503) <=> b.distanceCovered(2053)}.distanceCovered(2053)
-
-
+println lines
+        .collect {Reindeer.fromInput(it)}
+        .max {it.distanceCovered(RUNNING_TIME)}
+        .distanceCovered(RUNNING_TIME)
 
 
 
--- src/main/groovy/geoand/adventofcode/support/model/reindeer/Reindeer.groovy
+++ src/main/groovy/geoand/adventofcode/support/model/reindeer/Reindeer.groovy
@@ -1,23 +1,26 @@
 package geoand.adventofcode.support.model.reindeer
 
+import groovy.transform.CompileStatic
 import groovy.transform.Memoized
 import groovy.transform.PackageScope
+import groovy.transform.ToString
 
 import java.util.regex.Matcher
 
 /**
  * Created by gandrianakis on 22/1/2016.
  */
+@ToString(includes = 'name', includePackage = false)
 class Reindeer {
 
     final String name
-    final int speed
-    final int flightDuration
-    final int restDuration
+    final Integer speed
+    final Integer flightDuration
+    final Integer restDuration
 
     //visible for testing
     @PackageScope
-    Reindeer(String name, int speed, int flightDuration, int restDuration) {
+    Reindeer(String name, Integer speed, Integer flightDuration, Integer restDuration) {
         this.name = name
         this.speed = speed
         this.flightDuration = flightDuration
@@ -27,7 +30,7 @@ class Reindeer {
     static Reindeer fromInput(String input) {
         final Matcher matcher = (input =~ /(\w+)\s+can\s+fly\s+(\d+)\s+km\/s\s+for\s+(\d+)\s+seconds,\s+but\s+then\s+must\s+rest\s+for\s+(\d+)\s+seconds./)
         if(matcher.size() != 1) {
-            throw new IllegalArgumentException("input '${input}' was not is the expected format")
+            throw new IllegalArgumentException("input '${input}' was not in the expected format")
         }
 
         final def match = matcher[0]
@@ -36,11 +39,11 @@ class Reindeer {
     }
 
     @Memoized
-    int distanceCovered(int seconds) {
-        final int numberOfCompletedCycles = seconds / (flightDuration + restDuration)
-        final int numberOfSecondsInCurrentCycle = seconds % (flightDuration + restDuration)
+    @CompileStatic
+    Integer distanceCovered(Integer seconds) {
+        final Integer numberOfCompletedCycles = seconds.intdiv(flightDuration + restDuration) as Integer
+        final Integer numberOfSecondsInCurrentCycle = seconds % (flightDuration + restDuration)
 
         return speed * (numberOfCompletedCycles * flightDuration + (numberOfSecondsInCurrentCycle <= flightDuration ? numberOfSecondsInCurrentCycle : flightDuration))
-
     }
 }
